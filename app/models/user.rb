47619be1cf59a15a4d986313afb12e6a6b8a6387
Brook Nega <brooknega21@gmail.com>
2022-12-09 11:05:15 -0500

    user authenication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
     PASSWORD_REQUIREMENTS = /\A
         (?=.{8,}) # makes it min 8 char. long
         (?=.\d) # min 1 num
-        (?=.*[a-z]) # min 1 lower case
-        (?=.*[A-Z]) # min 1 upper case
-        (?=.*[[:^alnum:]]) # min one symbol
     /x
     validates :password, presence: true, format: PASSWORD_REQUIREMENTS
 
 end
+
+# (?=.*[a-z]) # min 1 lower case
+#         (?=.*[A-Z]) # min 1 upper case
+#         (?=.*[[:^alnum:]]) # min one symbol
